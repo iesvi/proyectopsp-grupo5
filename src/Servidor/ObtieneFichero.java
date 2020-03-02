c8a3cac6be5476b5a68cfb623b6777359e030d59
victorlopez <victorlopezarenal@gmail.com>
2020-03-02 20:36:28 +0100

    Añadido Servidor

---
+++ src/Servidor/ObtieneFichero.java
@@ -0,0 +1,17 @@
+package Servidor;
+
+import java.io.Serializable;
+
+public class ObtieneFichero implements Serializable {
+
+    byte[] contenidoFichero;
+
+    public ObtieneFichero(byte[] contenidoFichero){
+        this.contenidoFichero = contenidoFichero;
+    }
+
+    public byte[] getContenidoFichero(){
+        return contenidoFichero;
+    }
+}
+
